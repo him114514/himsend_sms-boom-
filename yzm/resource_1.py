9590bf14ab16c8bcc276831928965fb1ad9520cf
him technology <130269588+him114514@users.noreply.github.com>
2025-08-10 20:26:13 +0800

    Add files via upload

--- yzm/resource_1.py
+++ yzm/resource_1.py
@@ -1,5 +1,5 @@
 
-Get=['sxk']
+Get=['sxk','hc','rsk','cemo']
 def preparingnumber(number):
     game_url = 'https://m.bigplayers.com/api/user/v1/smsCode/sendLoginSmsV2'
     game_payload = {
@@ -180,8 +180,113 @@ def preparingnumber(number):
         "sec-fetch-site": "cross-site",
         "token": "",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0"
+
     }
     Zmn = [zmn_url, zmn_payload, zmn_headers]
 
-    conclusion={'game': Game,'hxx': Hxx,'kuaishou': Ks,'sdc': Sdc,'sjzg': Sjzg,'sxk': Sxk,'zmn': Zmn}
+    hc_url=f'https://auth.huace.cn/api/v1/customers/actions/telephone-captcha?telephone={number}&country=%2B86&type=login_type'
+    hc_payload = {
+        "telephone": number,
+        "country": "+86",
+        "type": "login_type"
+    }
+    hc_headers = {
+        "accept": "application/json, text/plain, */*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "clientid": "a446350f95de",
+        "connection": "keep-alive",
+        "content-language": "zh-CN",
+        "cookie": "CHC_LANGUAGE=zh-CN",
+        "host": "auth.huace.cn",
+        "referer": "https://auth.huace.cn/login?client_id=a446350f95de&client_code=cloud-survey-client&redirect=https%3A%2F%2Fcloud.huace.cn",
+        "sec-ch-ua": "\"Not;A=Brand\";v=\"99\", \"Microsoft Edge\";v=\"139\", \"Chromium\";v=\"139\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    Hc=[hc_url, hc_payload, hc_headers]
+
+    rsk_url=f'https://rsks.class.com.cn/v3/msg/front/commons/getCaptcha?mobile={number}'
+    rsk_payload = {
+        "mobile": number
+    }
+    rsk_headers = {
+        "accept": "application/json, text/plain, */*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "authorization": "Bearer",
+        "connection": "keep-alive",
+        "cookie": "HWWAFSESTIME=1754809700924; HWWAFSESID=341ad73f9a1ebaa7e3; Hm_lvt_1f060a73b23f9182af22ad065537307b=1754809704; HMACCOUNT=FF4E43BDD4FFE36E; Hm_lpvt_1f060a73b23f9182af22ad065537307b=1754809707",
+        "host": "rsks.class.com.cn",
+        "referer": "https://rsks.class.com.cn/register",
+        "sec-ch-ua": "\"Not;A=Brand\";v=\"99\", \"Microsoft Edge\";v=\"139\", \"Chromium\";v=\"139\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
+    }
+    Rsk=[rsk_url, rsk_payload, rsk_headers]
+
+    xz_url='https://xiezuocat.com/verify?type=signup'
+    xz_payload ={'phone': f"86-{number}"}
+    xz_headers = {
+        "accept": "application/json, text/plain, */*; charset=utf-8",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "cache-control": "no-cache",
+        "content-length": "26",
+        "content-type": "application/json",
+        "cookie": "sid=32905228298746c5be49fd1c595d4a22; traceid=6085cf62e12645da; _ga=GA1.1.395451313.1754811474; Hm_lvt_099c1a390e23e6b73b081c48519f6e8e=1754811475; Hm_lpvt_099c1a390e23e6b73b081c48519f6e8e=1754811475; HMACCOUNT=FF4E43BDD4FFE36E; JSESSIONID=D3EAA6E47332CEE83DD10077489AD1C3; _ga_NCW16CFM07=GS2.1.s1754811474$o1$g0$t1754811482$j52$l0$h0",
+        "origin": "https://xiezuocat.com",
+        "pragma": "no-cache",
+        "priority": "u=1, i",
+        "referer": "https://xiezuocat.com/",
+        "sec-ch-ua": "\"Not;A=Brand\";v=\"99\", \"Microsoft Edge\";v=\"139\", \"Chromium\";v=\"139\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
+    }
+    Xz=[xz_url, xz_payload, xz_headers]
+
+    cemo_url=f'https://passport.cnmo.com/index.php?c=Member_Ajax_Register&m=SendMessageCode&Jsoncallback=jQuery18309194080132428769_1754820202731&mobile={number}&type=5&codeimg=EE7F&_=1754820229348'
+    cemo_payload ={
+        "c": "Member_Ajax_Register",
+        "m": "SendMessageCode",
+        "Jsoncallback": "jQuery18309194080132428769_1754820202731",
+        "mobile": number,
+        "type": "5",
+        "codeimg": "EE7F",
+        "_": "1754820229348"
+    }
+    cemo_headers = {
+        "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "connection": "keep-alive",
+        "cookie": "PHPSESSID=b9vjoagrb6ch5m2t2mc4o31m13; cnmo_e476_LGTK=BTYSERZPBWROTPRPBIYOIAZPBGIOKCRDBGYFTE7NBIRI; cd35a471-68cb-4d42-83f4-6f5c27a3ca53=1a4f265089ae9db45f6ecc0851dc2cf4; cnmo_e476_post_seccode=a26f7NMQTHOJQE%2BkdvshM0RLdkzMl5PpUuGXfhIfLkys5gXYY2fOMTbA0OU",
+        "host": "passport.cnmo.com",
+        "referer": "https://passport.cnmo.com/",
+        "sec-ch-ua": "\"Not;A=Brand\";v=\"99\", \"Microsoft Edge\";v=\"139\", \"Chromium\";v=\"139\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    Cemo=[cemo_url, cemo_payload, cemo_headers]
+
+    conclusion={'game': Game,'hxx': Hxx,'kuaishou': Ks,'sdc': Sdc,'sjzg': Sjzg,'sxk': Sxk,
+                'zmn': Zmn,'hc': Hc,'rsk': Rsk,'xz': Xz,'cemo': Cemo}
     return conclusion
